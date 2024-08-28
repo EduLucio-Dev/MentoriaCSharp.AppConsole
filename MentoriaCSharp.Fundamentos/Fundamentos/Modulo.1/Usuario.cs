badeff9b98e3ade2fff028dc75841789ef70ee34
EduLucio-Dev <duio.el@gmail.com>
2024-08-28 18:21:41 -0300

    - ajuste de usuario git

--- MentoriaCSharp.Fundamentos/Fundamentos/Modulo.1/Usuario.cs
+++ MentoriaCSharp.Fundamentos/Fundamentos/Modulo.1/Usuario.cs
@@ -6,6 +6,6 @@
         // Programa que verifique a maior do usuario
         // Espera que o usuario digite sua idade
         // Resultado dis se é Criança, adolecente, adulto ou idoso.
-    
+        
     }
 }
